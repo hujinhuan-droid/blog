c3cb9b6550ae73bf5f9baf7e9de8d0d557f235b6
hujinhuan-droid <hujinhuan@hotmail.com>
2026-07-22 16:30:28 +0800

    feat: 导航与首页加入 Hugo 仓库外链入口

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,3 +1,6 @@
 // Site name is now fetched from server config at runtime
 // This is kept for backward compatibility but will be removed in the future
 export const siteName = ""
+
+// External GitHub repository links
+export const HUGO_REPO_URL = "https://github.com/hujinhuan-droid/hugo"
